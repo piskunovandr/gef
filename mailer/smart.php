d05806aecfb50a0f84fc10f27bfb760d821cb682
piskunovandr <piskunov2000andrey@gmail.com>
2021-08-04 20:39:13 +0500

    Финальный вариант

--- mailer/smart.php
+++ mailer/smart.php
@@ -5,7 +5,7 @@ $phone = $_POST['client-phone'];
 $email = $_POST['client-email'];
 
 require_once('phpmailer/PHPMailerAutoload.php');
-$mail = new PHPMailer();
+$mail = new PHPMailer;
 $mail->CharSet = 'utf-8';
 
 $mail->SMTPDebug = 3;                               // Enable verbose debug output
@@ -13,16 +13,13 @@ $mail->SMTPDebug = 3;                               // Enable verbose debug outp
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.mail.ru';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'mueno2020@mail.ru';                 // Наш логин
-$mail->Password = 'aa2440425aa';   
-//$mail->Username = 'gefest.vsk@bk.ru';                 // Наш логин
-//$mail->Password = 'Stroika2233!';                     // Наш пароль от ящика
+$mail->Username = 'gefest.vsk@bk.ru';                 // Наш логин
+$mail->Password = 'Stroika2233!';                     // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('mueno2020@mail.ru', 'Заявка на сайте ГЕФЕСТ ВСК');   // От кого письмо 
-$mail->addAddress('andrey.piskunov2111@mail.ru');     // Add a recipient
-//$mail->addAddress('info@gefestvsk.ru');     // Add a recipient
+$mail->setFrom('gefest.vsk@bk.ru', 'Заявка на сайте ГЕФЕСТ ВСК');   // От кого письмо 
+$mail->addAddress('info@gefestvsk.ru');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
